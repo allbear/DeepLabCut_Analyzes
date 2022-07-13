fd22884dafa89f5b488262667514f557f0dd01f4
alrab223 <allbear826@gmail.com>
2022-07-13 23:47:57 +0900

    機能追加

--- Dango_Analyzer/window/random_choice.py
+++ Dango_Analyzer/window/random_choice.py
@@ -7,9 +7,11 @@ class Random:
    sg.theme('BlueMono')
 
    def setup(self):
-      self.layout = [[sg.Text("フレーム抽出を行う動画を選択してください")],
+      self.layout = [[sg.Text("フレーム抽出を行う動画を選択してください", key="process")],
                      [sg.Button('ランダム抽出', key='random')],
                      [sg.Input('20', size=(4, 1), enable_events=True, key='random_num')],
+                     [sg.Radio(text='png', key="png", group_id='1', default=True)],
+                     [sg.Radio(text='jpg', key="jpg", group_id='1', default=False)],
                      [sg.Text('出力先', size=(15, 1)), sg.Input(), sg.FolderBrowse('フレームのフォルダを選択', key='output')],
                      [sg.Button('閉じる', key="Exit")]]
       self.window = sg.Window("フレーム抽出", self.layout, size=(800, 600), keep_on_top=True)
@@ -21,6 +23,11 @@ class Random:
          if event == 'Exit':
             break
          if event == "random":
-            frame_process.FrameProcess().frame_random(int(values['random_num']), values["output"], "png")
+            self.window["process"].update("抜き出しを開始します")
+            if values["png"] is True:
+               pic_type = "png"
+            else:
+               pic_type = "jpg"
+            frame_process.FrameProcess().frame_random(int(values['random_num']), values["output"], pic_type)
             self.window["process"].update("抜き出しが完了しました")
       self.window.Close()
